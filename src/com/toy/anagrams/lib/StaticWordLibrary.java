6725fdd91cd91de536fda14ab2668f2609c7429e
S528799 <S528799@nw43392.nwmissouri.edu>
2017-09-15 21:42:47 -0500

    first commit

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -37,6 +37,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
+        "sahu",
         "abstraction",
         "ambiguous",
         "arithmetic",
